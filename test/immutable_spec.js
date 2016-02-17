5469892f2614fc0b856a9c3631eb135ff12671fb
Josh Stupplebeen <jpstup@gmail.com>
2016-02-16 23:57:20 -0700

    Stubbing out restrictions/review structure

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,45 @@
 import {expect} from 'chai'
 
+const allergyTypes = ['vegan',
+                      'vegetarian',
+                      'ovo-vegan',
+                      'lacto-vegetarian',
+                      'lacto-ovo vegetarian',
+                      'pescetarian',
+                      'peanut',
+                      'tree nut'
+                      'soy',
+                      'gluten',
+                      'dairy',
+                      'egg',
+                      'wheat',
+                      'fish',
+                      'shellfish',
+                      'bahai',
+                      'buddhist',
+                      'christian',
+                      'hindu',
+                      'jewish',
+                      'muslim',
+                      'diabetics',
+                      'heart healthy',
+                      'low cholesterol']
+
+describe('a panera review', () {
+  let review = {
+    'panera':{
+      'gluten':[
+        'greek salad',
+        'classic salad'
+      ],
+      'vegan':[],
+      'vegetarian':[],
+      'ovo-vegan':[],
+      'lacto-vegetarian':[],
+    }
+  }
+})
+
 describe('immutability', () => {
   describe('number of reviews', () => {
     function increment(currentReviewCount) {
